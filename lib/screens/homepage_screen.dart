4b06bff422efdeaab4995945cc427c4b1fc38fd3
Miko <imran.zh.miko@gmail.com>
2021-06-15 01:26:21 +0600

    Finished primary structure of Home. Added placeholder images and dummy data.

--- lib/screens/homepage_screen.dart
+++ lib/screens/homepage_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../dummy_data/ImagesDummy.dart';
 
 class HomePageScreen extends StatelessWidget {
   const HomePageScreen({Key? key}) : super(key: key);
@@ -8,7 +9,110 @@ class HomePageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Eten'),),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 400,
+                  width: double.infinity,
+                  child: Image.asset(
+                    'Assets/Favorites.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  top: 55,
+                  width: MediaQuery.of(context).size.width,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Eten',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.wb_sunny_outlined,
+                            color: Colors.yellow[100],
+                            size: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ListTile(
+              leading: Text("Popular"),
+              trailing: Icon(Icons.stars),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(bottom: 5),
+                          width: 130,
+                          height: 150,
+                          child: Image.asset(
+                            list[index]['imageURL'] as String,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: list.length,
+              ),
+            ),
+            ListTile(
+              leading: Text("Random"),
+              trailing: Icon(Icons.stars),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(bottom: 5),
+                          width: 130,
+                          height: 150,
+                          child: Image.asset(
+                            list[index]['imageURL'] as String,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: list.length,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
