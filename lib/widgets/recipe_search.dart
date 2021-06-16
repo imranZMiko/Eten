45c5e07f9d4ccc193af885e307ac65af425eb10b
Miko <imran.zh.miko@gmail.com>
2021-06-17 00:58:06 +0600

    Changed recipe search to have a column with space between alignment.

--- lib/widgets/recipe_search.dart
+++ lib/widgets/recipe_search.dart
@@ -1,4 +1,5 @@
 import 'package:eten/widgets/search_results.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RecipeSearch extends StatefulWidget {
@@ -12,35 +13,43 @@ class _RecipeSearchState extends State<RecipeSearch> {
   var resultsShown = false;
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Row(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 10),
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          resultsShown = true;
-                        });
-                      },
-                      icon: Icon(Icons.search),
-                      splashRadius: 18,
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 10),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            resultsShown = true;
+                          });
+                        },
+                        icon: Icon(Icons.search),
+                        splashRadius: 18,
+                      ),
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter item name',
                     ),
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter item name',
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-        resultsShown ? Container(height: MediaQuery.of(context).size.height - 249.55,child: SearchResults(hasSearchBar: true),) : Container(),
-      ],
+            ],
+          ),
+          resultsShown
+              ? Container(
+                  height: MediaQuery.of(context).size.height - 249.55,
+                  child: SearchResults(hasSearchBar: true),
+                )
+              : Container(),
+        ],
+      ),
     );
   }
 }
