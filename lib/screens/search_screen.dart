5f26022c4e911dbb3657d67b2e8c82e026fae7bd
Miko <imran.zh.miko@gmail.com>
2021-06-16 19:40:38 +0600

    Added variable number of ingredient input fields

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -9,18 +9,20 @@ class SearchScreen extends StatefulWidget {
   _SearchScreenState createState() => _SearchScreenState();
 }
 
-class _SearchScreenState extends State<SearchScreen>
-    with SingleTickerProviderStateMixin {
+class _SearchScreenState extends State<SearchScreen> {
+  final _searchFormKey = GlobalKey<FormState>();
   var currentPage = 0;
+  var ingredientCount = 1;
 
   Container get currentScreen {
     if (currentPage == 1) {
+      ingredientCount = 1;
       return Container(
         width: 400,
         height: 80,
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-          child: TextField(
+          child: TextFormField(
             decoration: InputDecoration(
               contentPadding: EdgeInsets.only(left: 10),
               suffixIcon: IconButton(
@@ -36,29 +38,41 @@ class _SearchScreenState extends State<SearchScreen>
       );
     } else {
       return Container(
-        child: Row(
-          children: [
-            Container(
-              width: 340,
-              height: 80,
-              child: Padding(
-                padding: EdgeInsets.only(left:15, right:5, top: 20, bottom: 20),
-                child: TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 10),
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter ingredient',
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Row(
+              children: [
+                Container(
+                  width: 340,
+                  height: 80,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        left: 15, right: 5, top: 20, bottom: 5),
+                    child: TextFormField(
+                      enabled: index==ingredientCount-1?true:false,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(left: 10),
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter ingredient',
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.add),
-              iconSize: 23,
-              splashRadius: 18,
-            ),
-          ],
+                if(index == ingredientCount-1) IconButton(
+                  onPressed: () {
+                    setState(() {
+                      ingredientCount++;
+                    });
+                  },
+                  icon: Icon(Icons.add),
+                  iconSize: 23,
+                  splashRadius: 18,
+                ),
+              ],
+            );
+          },
+          itemCount: ingredientCount,
         ),
       );
     }
@@ -101,7 +115,11 @@ class _SearchScreenState extends State<SearchScreen>
             ],
           ),
         ),
-        body: currentScreen,
+        body: Form(
+          key: _searchFormKey,
+          autovalidateMode: AutovalidateMode.always,
+          child: currentScreen,
+        ),
       ),
     );
   }
