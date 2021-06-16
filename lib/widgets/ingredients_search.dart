8c544194046b82dfb447e66677b47571b6367a3b
Miko <imran.zh.miko@gmail.com>
2021-06-16 21:03:06 +0600

    Fixed add button being inaccessible with too many ingredients.

--- lib/widgets/ingredients_search.dart
+++ lib/widgets/ingredients_search.dart
@@ -19,37 +19,47 @@ class _IngredientsSearchState extends State<IngredientsSearch> {
           itemBuilder: (ctx, index) {
             return Padding(
               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
+              child: Column(
                 children: [
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 15),
-                      child: TextFormField(
-                        enabled: index == ingredientCount - 1 ? true : false,
-                        //autofocus: true,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 10),
-                          border: OutlineInputBorder(),
-                          hintText: 'Enter ingredient',
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              right: (index == ingredientCount - 1) ? 15 : 0),
+                          child: TextFormField(
+                            enabled:
+                                index == ingredientCount - 1 ? true : false,
+                            //autofocus: true,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 10),
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter ingredient',
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      if (index == ingredientCount - 1)
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              ingredientCount++;
+                            });
+                          },
+                          icon: Icon(Icons.add),
+                          iconSize: 23,
+                          splashRadius: 18,
+                          padding: EdgeInsets.all(8),
+                          constraints: BoxConstraints(),
+                        ),
+                    ],
                   ),
                   if (index == ingredientCount - 1)
-                    IconButton(
-                      onPressed: () {
-                        setState(() {
-                          ingredientCount++;
-                        });
-                      },
-                      icon: Icon(Icons.add),
-                      iconSize: 23,
-                      splashRadius: 18,
-                      padding: EdgeInsets.all(8),
-                      constraints: BoxConstraints(),
-                    ),
+                  Container(
+                    height: 90,
+                  ),
                 ],
               ),
             );
