6e55508e91df6fc8670e78ce00525ae33670225c
Miko <imran.zh.miko@gmail.com>
2021-06-20 03:45:26 +0600

    Added multiple results.

--- lib/widgets/search_results.dart
+++ lib/widgets/search_results.dart
@@ -21,12 +21,12 @@ class _SearchResultsState extends State<SearchResults> {
   String url =
       "https://api.spoonacular.com/recipes/findByIngredients?apiKey=3de7b3d7a06f401a8210e4c5a7f3ba7c&number=1&ingredients=";
 
-  late Future<Map<String, String>> _myFuture;
+  late Future<List<Map<String, String>>> _myFuture;
 
   @override
   void initState() {
     url =
-    "https://api.spoonacular.com/recipes/findByIngredients?apiKey=3de7b3d7a06f401a8210e4c5a7f3ba7c&number=1&ingredients=";
+        "https://api.spoonacular.com/recipes/findByIngredients?apiKey=3de7b3d7a06f401a8210e4c5a7f3ba7c&number=10&ranking=2&ingredients=";
     for (int i = 0; i < widget.ingredients.length; i++) {
       if (i != 0) url += ",+";
       url += "${widget.ingredients[i]}";
@@ -37,114 +37,128 @@ class _SearchResultsState extends State<SearchResults> {
     _myFuture = getData();
   }
 
-  Future<Map<String, String>> getData() async {
+  Future<List<Map<String, String>>> getData() async {
+    List<Map<String, String>> temp = [];
     print("Here");
     var response1 = await http.get(
       Uri.parse(Uri.encodeFull(url)),
     );
+    print(json.decode(response1.body));
 
-    var response2 = await http.get(Uri.parse(Uri.encodeFull(
-        "https://api.spoonacular.com/recipes/${json.decode(response1.body)[0]['id']}/information?includeNutrition=false&apiKey=3de7b3d7a06f401a8210e4c5a7f3ba7c")));
+    // (json.decode(response1.body) as List<Map<String, dynamic>>).forEach((element) async {
+    //   print(element);
+    for(int i = 0; i < 10; i++){
+      var response2 = await http.get(Uri.parse(Uri.encodeFull(
+          "https://api.spoonacular.com/recipes/${json.decode(response1.body)[i]['id']}/information?includeNutrition=false&apiKey=3de7b3d7a06f401a8210e4c5a7f3ba7c")));
+      temp.add({
+        'title': json.decode(response2.body)['title'],
+        'imageUrl': json.decode(response2.body)['image']
+      });
+    }
+    print(temp);
 
-    print(json.decode(response2.body));
-
-    return {'title':json.decode(response2.body)['title'], 'imageUrl':json.decode(response2.body)['image']};
+    return temp;
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       builder: (ctx, snapshot) {
-        if(snapshot.connectionState == ConnectionState.waiting)
-            return Center(
-                child: CircularProgressIndicator(),
-              );
+        if (snapshot.connectionState == ConnectionState.waiting)
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         else {
-          Map<String, String> data = snapshot.data! as Map<String, String>;
+          List<Map<String, String>> data =
+              snapshot.data! as List<Map<String, String>>;
           return ListView.builder(
-                itemBuilder: (ctx, index) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      if (index == 0)
-                        Padding(
-                          padding: EdgeInsets.all(widget.hasNoTitle ? 0 : 15),
-                          child: widget.hasNoTitle
-                              ? Container()
-                              : Text(
-                                  'Results',
-                                  style: TextStyle(fontSize: 20),
-                                ),
+            itemBuilder: (ctx, index) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (index == 0)
+                    Padding(
+                      padding: EdgeInsets.all(widget.hasNoTitle ? 0 : 15),
+                      child: widget.hasNoTitle
+                          ? Container()
+                          : Text(
+                              'Results',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                    ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder: (context, animation1, animation2) =>
+                              RecipeScreen(),
+                          transitionDuration: Duration(seconds: 0),
                         ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            PageRouteBuilder(
-                              pageBuilder: (context, animation1, animation2) =>
-                                  RecipeScreen(),
-                              transitionDuration: Duration(seconds: 0),
-                            ),
-                          );
-                        },
-                        child: Stack(
-                          children: [
-                            Container(
-                              width: MediaQuery.of(context).size.width,
-                              height: MediaQuery.of(context).size.width + 100,
-                              child: Image.network(
-                                data['imageUrl'] as String,
-                                fit: BoxFit.cover,
+                      );
+                    },
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.width + 100,
+                          child: Image.network(
+                            data[index]['imageUrl'] as String,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.width - 100,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                                colors: [
+                                  Colors.black87,
+                                  Colors.transparent,
+                                ],
                               ),
                             ),
-                            Positioned(
-                              bottom: 0,
-                              child: Container(
-                                width: MediaQuery.of(context).size.width,
-                                height: MediaQuery.of(context).size.width - 100,
-                                decoration: BoxDecoration(
-                                  gradient: LinearGradient(
-                                    begin: Alignment.bottomCenter,
-                                    end: Alignment.topCenter,
-                                    colors: [
-                                      Colors.black87,
-                                      Colors.transparent,
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              bottom: 5,
-                              child: Container(
-                                width: MediaQuery.of(context).size.width,
-                                child: Padding(
-                                  padding: EdgeInsets.all(15),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        data['title'] as String,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 18,
-                                        ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 5,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            child: Padding(
+                              padding: EdgeInsets.all(15),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    width: 300,
+                                    child: Text(
+                                      data[index]['title'] as String,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
                                       ),
-                                      FavoriteButton(),
-                                    ],
+                                    ),
                                   ),
-                                ),
+                                  FavoriteButton(),
+                                ],
                               ),
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ],
-                  );
-                },
-                itemCount: list.length,
-              );}
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+            itemCount: data.length,
+          );
+        }
       },
       future: _myFuture,
     );
